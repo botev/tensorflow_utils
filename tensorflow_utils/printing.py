ec460f553dfde76d67aba2b6f04a0875038f29e1
Aleksandar Botev <botevmg@gmail.com>
2018-02-12 14:29:49 +0000

    removed printing of parameters as that is now covered in native tensorflow.

--- tensorflow_utils/printing.py
+++ tensorflow_utils/printing.py
@@ -2,26 +2,17 @@ import tensorflow as tf
 
 
 __all__ = [
-    "print_params",
+    "load_parameters",
 ]
 
 
-def print_params(title,
-                 scope=None,
-                 key=tf.GraphKeys.GLOBAL_VARIABLES):
-    """
-    Prints all of the variables in the provided scope and the combined total
-    number of elements.
-
-    Args:
-        title: String.  A title name to print at the top.
-        scope: String.  Scope for which to print the variables.
-        key: GraphKey.  Which GraphKey to use.
-    """
-    print("\n{0:=<78}".format(title))
-    total = 0
-    for p in tf.get_collection(key, scope):
-        print(p.name, ":", p.shape)
-        total += p.shape.num_elements()
-    print("Total: {0:=<71,d}".format(total))
-    return total
+def load_parameters(saver, session, load_dir):
+    if tf.gfile.Exists(load_dir):
+        ckpt = tf.train.get_checkpoint_state(load_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(session, ckpt.model_checkpoint_path)
+        else:
+            tf.gfile.DeleteRecursively(load_dir)
+            tf.gfile.MakeDirs(load_dir)
+    else:
+        tf.gfile.MakeDirs(load_dir)
