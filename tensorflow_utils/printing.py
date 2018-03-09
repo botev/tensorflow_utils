f2f2daf08efed9426dd7482c57853f90e9518552
Aleksandar Botev <botevmg@gmail.com>
2018-03-09 21:38:49 +0000

    tiny fix for loading models trained in different paths.

--- tensorflow_utils/printing.py
+++ tensorflow_utils/printing.py
@@ -1,16 +1,21 @@
 import tensorflow as tf
 import shutil
+import os
 
 __all__ = [
     "load_parameters",
 ]
 
 
-def load_parameters(saver, session, load_dir, force_delete=False):
+def load_parameters(saver, session, load_dir, bootstrap_folder=True, force_delete=False):
     if tf.gfile.Exists(load_dir):
         ckpt = tf.train.get_checkpoint_state(load_dir)
         if ckpt and ckpt.model_checkpoint_path:
-            saver.restore(session, ckpt.model_checkpoint_path)
+            if bootstrap_folder:
+                path = os.path.join(load_dir, os.path.basename(ckpt.model_checkpoint_path))
+            else:
+                path = ckpt.model_checkpoint_path
+            saver.restore(session, path)
         elif force_delete:
             shutil.rmtree(load_dir)
             tf.gfile.MakeDirs(load_dir)
