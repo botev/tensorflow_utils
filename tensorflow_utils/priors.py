df9c7ce2e5b0879b0199f7f511a1c7617216f165
Alex Botev <botevmg@gmail.com>
2018-05-29 00:51:55 -0700

    Added double-mixture prior.

--- tensorflow_utils/priors.py
+++ tensorflow_utils/priors.py
@@ -151,6 +151,7 @@ def gaussian_regularizer(mode="FAN_AVG",
         raise TypeError('Unknown mode %s [FAN_IN, FAN_OUT, FAN_AVG]', mode)
 
     scale_factor = tf.cast(scale_factor, tf.float32)
+
     # For a single tensor
     def _log_prob_single(tensor):
         stddev = tf.sqrt(scale_factor / calculate_variance_factor(tensor.shape, mode))
@@ -163,6 +164,125 @@ def gaussian_regularizer(mode="FAN_AVG",
             invalid = tf.logical_or(tf.less_equal(z, -2), tf.greater_equal(z, 2))
             log_prob = tf.where(invalid, -inf, log_prob)
         # Return negative as this is a regularizer
+        return - log_prob
+
+    # For one or more tensors
+    def _log_prob_fn(tensors):
+        if isinstance(tensors, (list, tuple)):
+            return sum(_log_prob_single(tensor) for tensor in tensors)
+        else:
+            return _log_prob_single(tensors)
+
+    return _log_prob_fn
+
+
+def double_mixture_initializer(mode="FAN_AVG",
+                               scale_factor=0.5,
+                               mean=0.0,
+                               truncated=False,
+                               seed=None,
+                               dtype=tf.float32):
+    """
+    Returns an initializer that generates tensors from a mixture of two
+    gaussian distributions.
+
+    The initializer uses the following formula for calculating the variance of
+    the gaussian distribution:
+
+        variance = scale_factor / mode_factor
+        mean = +/- 3 * sqrt(variance)
+
+    Args:
+        mode: String.  See `calculate_variance_factor`
+        truncated: Bool.  Whether to use truncated gaussian instead of normal.
+        scale_factor: Float.  Additional multiplicative scale of the variance.
+        mean: Float.  Mean of the distribution.
+        seed: A Python integer. Used to create random seeds. See
+              @{tf.set_random_seed} for behavior.
+        dtype: The data type. Only floating point types are supported.
+
+    Returns:
+        An initializer that generates tensors from a gaussian distribution.
+
+    Raises:
+        TypeError: if `dtype` is not a floating point type.
+        TypeError: if `mode` is not in ['NULL', 'FAN_IN', 'FAN_OUT', 'FAN_AVG'].
+        ValueError: if `scale_factor` is an integer.
+    """
+    scale_factor = tf.convert_to_tensor(scale_factor, dtype=tf.float32)
+    if not dtype.is_floating:
+        raise TypeError("Cannot create initializer for non-floating point type.")
+    if mode not in ["NULL", "FAN_IN", "FAN_OUT", "FAN_AVG"]:
+        raise TypeError("Unknown mode %s [NULL, FAN_IN, FAN_OUT, FAN_AVG]", mode)
+
+    def _initializer(shape, dtype=dtype, partition_info=None):
+        if not dtype.is_floating:
+            raise TypeError("Cannot create initializer for non-floating point type.")
+        stddev = tf.sqrt(scale_factor / calculate_variance_factor(shape, mode))
+        if truncated:
+            s1 = tf.truncated_normal(shape, mean - 3 * stddev, stddev, dtype, seed=seed)
+            s2 = tf.truncated_normal(shape, mean + 3 * stddev, stddev, dtype, seed=seed)
+        else:
+            s1 = tf.random_normal(shape, mean - 3 * stddev, stddev, dtype, seed=seed)
+            s2 = tf.random_normal(shape, mean + 3 * stddev, stddev, dtype, seed=seed)
+        index = tf.cast(tf.random_uniform(shape, seed=seed) < 0.5, dtype=dtype)
+        return index * s1 + (1.0 - index) * s2
+    return _initializer
+
+
+def double_mixture(mode="FAN_AVG",
+                   scale_factor=1.0,
+                   mean=0.0,
+                   truncated=False):
+    """
+    Returns a regularizer based on the log probability of the equivalent
+    mixture of two gaussian distributions initializer.
+
+    The regularizer uses the following formula for calculating the variance of
+    the gaussian distribution:
+
+        variance = scale_factor / mode_factor
+        mean = +/- 3 * sqrt(variance)
+
+    Args:
+        mode: String.  See `calculate_variance_factor`
+        truncated: Bool.  Whether to use truncated gaussian instead of normal.
+        scale_factor: Float.  Additional multiplicative scale of the variance.
+        mean: Float.  Mean of the distribution.
+
+    Returns:
+        A regularizer that generates tensors from a gaussian distribution.
+
+    Raises:
+        TypeError: if `dtype` is not a floating point type.
+        ValueError: if `scale_factor` is an integer.
+    """
+    if mode not in ["NULL", "FAN_IN", "FAN_OUT", "FAN_AVG"]:
+        raise TypeError('Unknown mode %s [FAN_IN, FAN_OUT, FAN_AVG]', mode)
+    scale_factor = tf.cast(scale_factor, tf.float32)
+
+    # For a single tensor
+    def _log_prob_single(tensor):
+        stddev = tf.sqrt(scale_factor / calculate_variance_factor(tensor.shape, mode))
+        z1 = (tensor - mean - 3 * stddev) / stddev
+        log_prob_z1 = - (z1 ** 2 + tf.log(2 * pi)) / 2
+        log_prob1 = tf.reduce_sum(log_prob_z1)
+        z2 = (tensor - mean + 3 * stddev) / stddev
+        log_prob_z2 = - (z2 ** 2 + tf.log(2 * pi)) / 2
+        log_prob2 = tf.reduce_sum(log_prob_z2)
+        if truncated:
+            from numpy import inf
+            log_prob1 -= tf.log(TRUNCATED_NORMALIZER)
+            invalid = tf.logical_or(tf.less_equal(z1, -2), tf.greater_equal(z1, 2))
+            log_prob1 = tf.where(invalid, -inf, log_prob1)
+            log_prob2 -= tf.log(TRUNCATED_NORMALIZER)
+            invalid = tf.logical_or(tf.less_equal(z2, -2), tf.greater_equal(z2, 2))
+            log_prob2 = tf.where(invalid, -inf, log_prob2)
+        # Return negative as this is a regularizer
+        m = tf.maximum(log_prob1, log_prob2) - tf.log(2.0)
+        log_prob1 -= m
+        log_prob2 -= m
+        log_prob = m + tf.log(tf.exp(log_prob1) + tf.exp(log_prob2))
         return - log_prob
 
     # For one or more tensors
